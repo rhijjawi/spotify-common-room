38db1e53b261e407d24a7c547b454939e213d67f
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2022-08-31 18:49:34 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 let $ = (selector) => document.querySelector(selector);
+async function addToQueue(e) {
+  let r = await axios.post('http://127.0.0.1:5555/queue', {'uri' : e})
+  console.log(r.data)
+}
 $('#form').addEventListener('submit', async (e)=>{
   console.log(e);
   e.preventDefault();
@@ -7,10 +11,35 @@ $('#form').addEventListener('submit', async (e)=>{
   let index = 0;
   for (i of p.data.tracks.items) {
     index++
-    if (index <= 10) {
-      $(`#albumartimg${index}`).src = i.album.images[1].url
-      $(`#trackTitle${index}`).innerHTML = i.name
-    }
+    let div = document.createElement('div');
+    let albumart = document.createElement('div');
+    let albumartimg = document.createElement('img');
+    let titleCard = document.createElement('div');
+    let titleCardText = document.createElement('span');
+    let queueButtondiv = document.createElement('div');
+    let queueButton = document.createElement('button');
+
+    titleCard.className = 'titleCard';
+    queueButtondiv.className = 'addtoqueue';
+    queueButton.className = 'addtoqueuebtn';
+    queueButton.style.width = '100%';
+    queueButton.dataset.uri = i.uri
+    queueButton.onclick = (e) => {addToQueue(e.srcElement.dataset.uri)}
+    albumart.className = 'albumart';
+    titleCardText.innerHTML = i.name;
+    titleCardText.style.fontSize = 'small';
+    albumartimg.src = i.album.images[1].url;
+    queueButtondiv.appendChild(queueButton);
+    titleCard.appendChild(titleCardText);
+    albumart.appendChild(albumartimg);
+    queueButton.innerHTML = 'Add to queue';
+    div.appendChild(albumart);
+    div.appendChild(titleCard);
+    div.appendChild(queueButtondiv);
+    div.className = 'column-4';
+    console.log(div.className)
+    $('#search_box').appendChild(div);
   }
+  $('#searchResults').hidden = false;
   return true;
 });
