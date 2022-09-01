72994789801f397482b2ffc219db35f40de3e820
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2022-09-01 03:00:15 +0300

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, abort
+from flask import Flask, request, make_response, abort, redirect
 from flask_cors import CORS
 import requests
 import json
@@ -10,9 +10,13 @@ clientID = '081b69bc044e41d59d9d28bb6c8a4305'
 clientSec = '9f61c2a752f14404959fc989e0d2cbef'
 l = f'{clientID}:{clientSec}'
 l = l.encode("utf-8")
+print(base64.b64encode(l))
 accessTokenTime = 0
 access_token = None
 
+accessToken = None
+refreshToken = None
+refreshTime = None
 
 def getToken():
     global accessTokenTime
@@ -26,17 +30,39 @@ def getToken():
     else:
         return access_token
 
+def getRefreshedToken():
 
+        if code.status_code == 200:
+            refreshTime = time.time()
+            accessToken = code.json()['access_token']
+            refreshToken = code.json()['refresh_token']
+    pass
 
 @app.route('/search', methods=["POST"])
 def search():
     search = requests.get(f'https://api.spotify.com/v1/search?type=track,album&market=UG&include_external=audio&q={request.json["query"]}', headers={'Authorization': f'Bearer {getToken()}'})
     return search.json()
 
-@app.route('/queue', methods=["POST"])
+@app.route('/queue', methods=["POST", "GET"])
 def addtoqueue():
-    queue = requests.get(f'https://api.spotify.com/v1/me/player/devices', headers={'Authorization': f'Bearer {getToken()}'})
+    if request.method == "POST":
+        queue = requests.get(f'https://api.spotify.com/v1/me/player/devices', headers={'Authorization': f'Bearer {getToken()}'})
+    elif request.method == "GET":
+        queue = requests.get(f'https://api.spotify.com/v1/me/player/queue')
     return queue.json()
+
+@app.route('/login')
+def login():
+    return redirect('https://accounts.spotify.com/authorize?client_id=081b69bc044e41d59d9d28bb6c8a4305&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5555%2Fcallback&scope=user-modify-playback-state%20user-read-playback-state%20user-read-currently-playing%20user-read-playback-position%20&show_dialog=false')
+
+@app.route('/callback')
+def callback():
+    global accessToken
+    global refreshToken
+    print(request.args.get("code"))
+    code = requests.post(f'https://accounts.spotify.com/api/token?grant_type=authorization_code&code={request.args.get("code")}&redirect_uri=http%3A%2F%2Flocalhost%3A5555%2Fcallback', headers={'Authorization': f'Basic {base64.b64encode(l).decode("utf-8")}', 'Content-Type':'application/x-www-form-urlencoded'})
+    
+    return code.json()
 
 @app.route('/', methods=["POST"])
 def main():
